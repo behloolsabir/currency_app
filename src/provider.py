4b2d6e453f92cb666f406d34ac8134ad844911c8
behloolsabir <behloolsabir@gmail.com>
2021-06-19 13:12:37 +0530

    oop changes

---
+++ src/provider.py
@@ -0,0 +1,9 @@
+class provider:
+    """
+    Provider object.
+    """
+    def __init__(self, url):
+        self.url = url
+    
+    def parseURL(self):
+        
---
+++ src/trade.py
@@ -0,0 +1,14 @@
+class trade:
+    """
+    Trade object.
+    """
+    def __init__(self, base, quote, timestamp, provider, rate):
+        self.base = base
+        self.quote = quote
+        self.timestamp = timestamp
+        self.provider = provider
+        self.rate = rate
+    
+    def transact(self):
+    def buy(self):
+    def sell(self):
